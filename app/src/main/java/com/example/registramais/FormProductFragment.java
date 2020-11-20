202fa90ec0a905e4877bc5d502439401defafb99
joao.230887 <joaopaulo.ra04@gmail.com>
2020-11-20 17:47:39 -0300

    update formProduct

--- app/src/main/java/com/example/registramais/FormProductFragment.java
+++ app/src/main/java/com/example/registramais/FormProductFragment.java
@@ -39,7 +39,6 @@ public class FormProductFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_form_product, container, false);
         carregaCampos(view);
 
